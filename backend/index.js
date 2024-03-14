dc3af4d431ccc5fce647fa2569d1dd18df866348
bahriilhame <bahri.ilhame@gmail.com>
2024-03-14 12:57:17 +0000

    added the connection & created database with mongoose

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express  = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT=8080 
+
+mongoose.connect("mongodb://127.0.0.1:27017/cloud_stg")
+.then(()=>{
+    console.log("Server is running on port :",PORT);
+    app.listen(PORT)
+})
+.catch((err)=>console.log(err))
+
+const schemaData  = mongoose.Schema({
+    nom : String,
+    prenom : String,
+    age : Number,
+    adresse:String,
+    genre:String,
+    niveau:String,
+    groupe:String,
+    anneeScolaire:String
+},{
+    timestamps : true
+})
+
+const stagiaireCollection  = mongoose.model("stagiaire",schemaData)
